b2bda06ac8cf10e556ed6d76b83a6e5e7a979b46
akashnautiyal013 <akash.nautiyal013@gmail.com>
2015-09-18 11:31:34 +0530

    firstcommitforweb

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sharingsmilesinApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
